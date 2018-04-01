6d7a5b2afdc4817e90acb0b62585794cb9402152
abhilashsn <abhilash.h@relyonsoft.com>
2018-04-01 12:23:06 +0530

    adds floor model and migation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180324070340) do
+ActiveRecord::Schema.define(version: 20180401064012) do
 
   create_table "customers", force: :cascade do |t|
     t.string "empname"
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 20180324070340) do
     t.integer "perm_res_no"
     t.string "perm_res_name"
     t.string "perm_street"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "floors", force: :cascade do |t|
+    t.string "floor_name"
+    t.integer "number_of_rooms"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
